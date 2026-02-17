10f0397811aa7830e18a52d83725cf53a2414fba
AlgoRythm <luigi.distefano69@gmail.com>
2026-02-17 17:16:34 +0100

    end 27/06/2026

--- 27_06_2026_loading_2/main.c
+++ 27_06_2026_loading_2/main.c
@@ -22,14 +22,33 @@ typedef struct l {
 	Node* tail;
 } List;
 
+void error(char* );
+Parameters readInput(int ,char* []);
+void initializeList(List* );
+void insertRecord(List* , float f, char* s);
+void buildList(List* , char*);
+void printList(List*);
+void elab(float, List*);
+
 int main(int argc,char* argv[])
 {
-	printf("====================A====================");
+	printf("\n====================A====================");
+	Parameters p = readInput(argc, argv);
+	printf("\nstring: %s, float: %f", p.inputFilename, p.f);
+
+	printf("\n====================B====================");
+	printf("Loading PrintList");
 	
-	printf("====================B====================");
-	printf("====================C====================");
-	printf("====================D====================");
+	List list;
+	initializeList(&list);
+
+	buildList(&list, p.inputFilename);
+
+	printf("\n====================C====================");
+	printList(&list);
 	
+	printf("\n====================D====================");
+	elab(p.f, &list);
 	
 }
 
@@ -59,25 +78,115 @@ Parameters readInput(int argc, char* argv[])
 	{
 		error("float not correct");
 	}
-	
-	// p.inputFilename = malloc(strlen(inputFilename) + 1);
-	// strcpy(p.inputFilename, inputFilename)
-	//p.f = f;
-	
+		
 	Parameters p = {inputFilename, f};
 	
 	return p;
-
 }
 
-void insertRecord(List *list)
+void initializeList(List *list)
 {
-	
+	list->head = NULL;
+	list->tail = NULL;
+
+	printf("List initialized");
+
+	return;
 }
 
-void buildList(List *list, Record *record)
+void insertRecord(List *list, float f, char* s)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	if (!newNode)
+		error("Error during node allocation");
+
+	//Diamo identità e contenuto al nuovo nodo
+	newNode->f = f;
+	strcpy(newNode->string, s);
+
+	newNode->next = NULL;
+	newNode->prev = NULL;
+	//Dobbiamo implementare il comportamento secondo il quale l'elemento si va a posizionare in ordine lessiografico crescente
+	
+	//Caso 1: Lista vuota
+	if (list->head == NULL)
+	{
+		list->head = newNode;
+		list->tail = newNode;
+		printf("Inserito primo elemento in una lista vuota");
+		return;
+	}
+
+	//Cerca la poszione per dove essere messo il nuovo nodo rispettando l'ordine lessiografico crescente
+	Node* current = list->head;
+
+	while(current != NULL && strcmp(newNode->string, current->string) >= 0)
+	{
+		current = current->next;
+	}
+
+	//Se current è == NULL, significa che newNode è il più grande quindi va in Coda
+	if (current == NULL)
+	{
+		newNode->prev = list->tail;
+		list->tail->next = newNode;
+		list->tail = newNode; //Aggiorniamo la tail
+	}
+	//Se current->prev == NULL significa che il nostro nuovo nodo viene prima della testa
+	else if (current->prev == NULL)
+	{
+		newNode->next = current; //Questa sarebbe la testa list->head prima di sostituirla
+		current->prev = newNode;
+		list->head = newNode; //Head aggiornata
+	}
+	//Ultimo caso siamo nel Mezzo, quindi non dobbiamo aggiornare nè head nè tail
+	//Quindi devo inserere tra current->perv e current
+	else
+	{
+		newNode->next = current;
+		newNode->prev = current->prev;
+		current->prev->next = newNode;
+		current->prev = newNode;
+	}
+	
+	printf("Inserito primo elemento in lista");
+
+
+}
+
+void buildList(List *list, char* inputFilename)
 {
 	//legge le stringhe dal file
+	FILE* f = fopen(inputFilename, "r");
+	
+	char buffer[MAX_STRING_SIZE];
+	char bufferString[MAX_STRING_SIZE];
+	float bufferFloat;
+
+	int i = 1;
+	while (fgets(buffer, MAX_STRING_SIZE,f) != NULL)
+	{
+		//Se ci troviamo in posizione dispari torveremo il float
+		if (i % 2 != 0)
+		{
+			bufferFloat = atof(buffer);
+			printf("\nFloat trovato: %f", bufferFloat);
+
+			i++;
+		}
+		//Sennò troveremo la stringa e ogni volta che trovo la string inserisco il record e resetto i buffer
+		else
+		{
+			strcpy(bufferString, buffer);
+			printf("\nString trovata: %s", bufferString);
+			//strcspn restituisce la prima occorrenza di ciò che mettiamo come secondo argomento
+			bufferString[strcspn(bufferString, "\n")] = '\0';
+			insertRecord(list, bufferFloat, bufferString);
+			i++;
+		}
+
+	}
+	
 	
 	//le inserisce una alla volta mendiante insertRecord()
 	
@@ -85,34 +194,48 @@ void buildList(List *list, Record *record)
 
 void printList(List *list)
 {
-	//Funzione che stampa l'intera lista sullo standard output dal primo all'ultimo elemento
-}
-
-
-void elab()
-{
-	//Funzione che stampa il numero di record tali che V >= F;
-}
-
-void push(List *list, char *s,float f)
-{
-	//Creiamo il nuovo nodo
-	Node *newNode = (Node*)malloc(sizeof(Node));
-	
-	strcpy(newNode->string, s);
-	newNode->f = f;
-	
-	//Inserisco nella test (Comportamento LIFO)
-	newNode->next = list->head;
-	newNode->prev = NULL;
-	
-	if (list->head != NULL)
+	if (list->head == NULL)
 	{
-		list->head->prev = newNode;
-	} else {
-		//Lista vuola: head e tail sono lo stesso nodo
-		list->tail = newNode;
+		printf("Lista vuota");
+		return;
 	}
-	
-	list->head = newNode; // Aggiorno la testa 
+
+	printf("\nStampo la Lista...");
+	int i = 0;
+	Node* current = list->head;
+
+	while (current != NULL)
+	{
+		printf("\nRecord %d: | %s | %f |\n", i, current->string, current->f);
+		current = current->next;
+		i++;
+	}
+
+	printf("\nFine stampa...");
+
+}
+
+//Funzione che stampa il numero di record tali che V >= F;
+void elab(float f, List* list)
+{
+	if (list->head == NULL)
+	{
+		printf("Lista vuota");
+		return;
+	}
+
+	printf("\nElab in corso...");
+	int i = 0;
+	Node* current = list->head;
+
+	while (current != NULL)
+	{
+		if (current->f >= f)
+		{
+			i++;
+		}
+		current = current->next;
+	}
+
+	printf("\nFine elab, numero di record tali che V >= %f è %d", f, i);
 }
