351470ca316f4b5a0072d88c1e91600c11493040
AlgoRythm <luigi.distefano69@gmail.com>
2026-02-19 08:59:08 +0100

    start sorting study

---
+++ sorting-type/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE 10;
+
+void bubbleStort(int []);
+void isertionSort(int []);
+void selectionSort(int []);
+
+int main(int argc, char* argv[])
+{
+    int matrix[10] = {10, 28, 40, 32, 76, 20, 30, 1, 5, 8};
+}
+
+void bubbleSort(int matrix[])
+{
+    
+}
+
+void isertionSort(int matrix[])
+{
+
+}
+
+void selectionSort(int matrix[])
+{
+
+}
