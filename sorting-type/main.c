1388528293a26f2b3ff5675011c544d2e4c7ed58
AlgoRythm <luigi.distefano69@gmail.com>
2026-02-19 09:28:22 +0100

    sorting to implement

--- sorting-type/main.c
+++ sorting-type/main.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARRAY_SIZE 10;
+#define ARRAY_SIZE 10
 
-void bubbleStort(int []);
-void isertionSort(int []);
-void selectionSort(int []);
+void bubbleStort(int [], int);
+void isertionSort(int [], int);
+void selectionSort(int [], int);
+void printArray(int [], int);
 
 int main(int argc, char* argv[])
 {
-    int matrix[10] = {10, 28, 40, 32, 76, 20, 30, 1, 5, 8};
+    int original[10] = {10, 28, 40, 32, 76, 20, 30, 1, 5, 8};
+    int matrix[ARRAY_SIZE];
+
+    printf("\nArray originale:");
+    printArray(original, ARRAY_SIZE);
+
 }
 
-void bubbleSort(int matrix[])
+void bubbleSort(int matrix[], int n)
 {
+
+}
+
+void isertionSort(int matrix[], int n)
+{
+
+}
+
+void selectionSort(int matrix[], int n)
+{
+
+}
+
+void printArray(int matrix[], int n)
+{
+    printf("\n*** Stampo array ***");
     
-}
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ,", matrix[i]);
+    }
 
-void isertionSort(int matrix[])
-{
-
-}
-
-void selectionSort(int matrix[])
-{
-
+    printf("\n*** Fine stampa array ***");
 }
